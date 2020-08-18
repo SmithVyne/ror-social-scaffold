eb725e60b91571887fbfd02965021eedcb5ee863
Paul Omondi <paulo.techie@gmail.com>
2020-08-19 01:35:42 +0300

    add like unit tests

---
+++ spec/models/like_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  context 'validation tests'
+
+  before :each do
+    @user = User.create(name: 'paul', email: 'paulo@microverse.org', password: 'password')
+    @post = Post.create(user_id: @user.id, content: 'This is is a post')
+  end
+
+  it 'should initialize a like' do
+    like = Like.new(user_id: @user.id, post_id: @post.id)
+    expect(like.save).to eq(true)
+  end
+end
