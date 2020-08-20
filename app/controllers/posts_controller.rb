ce1f30f7fed7ceae8e523c706e20a0b034554a97
SmithVyne <smithnkereuwem2@gmail.com>
2020-08-20 18:29:49 +0300

    add required changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
 
   def index
     @post = Post.new
-    timeline_posts
+    @timeline_posts = current_user.timeline_posts
   end
 
   def create
@@ -12,19 +12,12 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to posts_path, notice: 'Post was successfully created.'
     else
-      timeline_posts
+      @timeline_posts
       render :index, alert: 'Post was not created.'
     end
   end
 
   private
-
-  def timeline_posts
-    users_posts = current_user.posts
-    friends_posts = current_user.friends.map { |friend| friend.posts if friend.posts.any? }.compact
-    friends_posts = friends_posts.flatten
-    @timeline_posts = users_posts + friends_posts
-  end
 
   def post_params
     params.require(:post).permit(:content)
