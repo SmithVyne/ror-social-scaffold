f177648b9a1a86b7eca7bd121e29096b8b719d67
milypm <mp_morones@hotmail.com>
2021-03-02 20:52:04 -0600

    Fix linters

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,10 +1,12 @@
 module FriendshipsHelper
   def show_friendship_btn
-    if Friendship.find_by(user_id: current_user.id, friend_id: @user.id) || Friendship.find_by(user_id: @user.id, friend_id: current_user.id)
+    if Friendship.find_by(user_id: current_user.id, friend_id: @user.id)
       return
-    end
-
-    unless current_user.id == @user.id
+    elsif Friendship.find_by(user_id: @user.id, friend_id: current_user.id)
+      return
+    else
+      return if current_user.id == @user.id
+      
       link_to 'Add Friend', friendships_path(friend_id: @user.id), method: :post, class: 'btn btn-success pt-2'
     end
   end
@@ -12,13 +14,12 @@ module FriendshipsHelper
   def index_friendship_btn(user)
     if Friendship.find_by(user_id: current_user.id, friend_id: user.id)
       return
-    elsif
-      Friendship.find_by(user_id: user.id, friend_id: current_user.id)
+    elsif Friendship.find_by(user_id: user.id, friend_id: current_user.id)
       return
     else
-      unless current_user.id == user.id
-        link_to 'Add Friend', friendships_path(friend_id: user.id), method: :post, class: 'mb-2 btn-sm btn btn-success'
-      end
+      return if current_user.id == user.id      
+      
+      link_to 'Add Friend', friendships_path(friend_id: user.id), method: :post, class: 'mb-2 btn-sm btn btn-success'
     end
   end
 
